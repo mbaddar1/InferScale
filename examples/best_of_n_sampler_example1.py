52920e1f31bcbc9878f8cc5b4cf7058a65906a7d
mbaddar <mbaddar2@gmail.com>
2026-03-03 22:03:23 +0100

    now we have error of index out of range - to investigate

--- examples/best_of_n_sampler_example1.py
+++ examples/best_of_n_sampler_example1.py
@@ -5,6 +5,10 @@ if __name__ == '__main__':
     evaluation_metric = "bert_score"
     bon = BestOfNSampler(models_names=models_names,evaluation_metric=evaluation_metric)
     dataset = load_dataset("cnn_dailymail", "3.0.0")
+    # FIXME , this line leads to this error
+    """
+        dev_log/dev_log.txt
+    """
     queries = [dataset["train"][0]["article"],dataset["train"][1]["article"],dataset["train"][2]["article"]]
     results = bon.generate(queries=queries,n=3)
     print(results)
