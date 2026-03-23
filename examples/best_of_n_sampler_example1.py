54d522d1aa74634a6f212b62906a26ed67cbd3a2
mbaddar <mbaddar2@gmail.com>
2026-03-23 12:12:50 +0200

    simplifications (1) One model (2) retrun all candidates

--- examples/best_of_n_sampler_example1.py
+++ examples/best_of_n_sampler_example1.py
@@ -1,8 +1,9 @@
 from inferscale.best_of_n import BestOfNSampler
 from datasets import load_dataset
 if __name__ == '__main__':
-    models_names = ["Sachin21112004/distilbart-news-summarizer","google/pegasus-xsum"]
-    bon = BestOfNSampler(models_names=models_names)
+    # models_names = ["Sachin21112004/distilbart-news-summarizer","google/pegasus-xsum"]
+    model_name = "Sachin21112004/distilbart-news-summarizer"
+    bon = BestOfNSampler(model_name=model_name)
     dataset = load_dataset("cnn_dailymail", "3.0.0")
     queries = [dataset["train"][0]["article"],dataset["train"][1]["article"],dataset["train"][2]["article"]]
     results = bon.generate(queries=queries,n=3)
--- src/inferscale/best_of_n/best_of_n_sampler.py
+++ src/inferscale/best_of_n/best_of_n_sampler.py
@@ -37,7 +37,7 @@ https://huggingface.co/docs/trl/main/en/best_of_n
 class BestOfNSampler:
     SUPPORTED_MODELS = ["Sachin21112004/distilbart-news-summarizer", "google/pegasus-xsum"]
 
-    def __init__(self, models_names: List[str]):
+    def __init__(self, model_name:str):
         """
             Best-of-N inference sampler.
 
@@ -46,20 +46,15 @@ class BestOfNSampler:
 
             Parameters
             ----------
-            models_names : list[str]
+            model_name : list[str]
                 List of model names used for generation.
             """
-        for model_name in models_names:
+        if model_name in BestOfNSampler.SUPPORTED_MODELS:
             if model_name not in self.SUPPORTED_MODELS:
                 raise ValueError("Invalid model: {}".format(model_name))
-        self.models_names = models_names
-        self.models = []
-        self.tokenizers = []
-        for model_name in models_names:
-            model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
-            tokenizer = AutoTokenizer.from_pretrained(model_name)
-            self.models.append(model)
-            self.tokenizers.append(tokenizer)
+        self.model_name = model_name
+        self.model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.eval_embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
         logger.info("BestOfNSampler successfully initialized")
 
@@ -68,20 +63,13 @@ class BestOfNSampler:
         #   1. Use lambda map to reduce loops
         #   2. Support parallel processing (parallel sampling)
         #   3. Smart model selector (based on query intent identification and the task at hand
-        best_results = []
+        # best_results = []
         for u, q in tqdm(enumerate(queries), desc="queries"):
-            max_score = -np.inf
-            best_candidate = None
-            best_model = None
-            for j in tqdm(range(len(self.models)), desc="models"):
-                candidates = []
-                for i in tqdm(range(n), desc="response-samples"):
-                    tokenizer = self.tokenizers[j]
-                    model = self.models[j]
-                    inputs = tokenizer(q, return_tensors="pt", max_length=1024, truncation=True)
-                    try:
-                        summary_ids = model.generate(
-                            inputs["input_ids"],
+            candidates = []
+            for i in tqdm(range(n), desc="response-samples"):
+                inputs = self.tokenizer(q, return_tensors="pt", max_length=1024, truncation=True)
+                try:
+                    summary_ids = self.model.generate(inputs["input_ids"],
                             max_length=150,
                             min_length=40,
                             no_repeat_ngram_size=3,
@@ -92,23 +80,30 @@ class BestOfNSampler:
                             temperature=1.5,
                             early_stopping=True
                         )
-                        result = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
-                        candidates.append(result)
-                    except Exception as e:
-                        logger.error(
-                            f"Exception : For query = {q}, model = {self.models_names[j]}, sample # {i},exception = {e}")
-
-                if len(candidates) > 0:
-                    doc_emb = self.eval_embedding_model.encode([q] * n)
-                    sum_emb = self.eval_embedding_model.encode(candidates)
-                    eval_score = cosine_similarity(sum_emb, doc_emb)[:, 0]
-                    max_eval_score_index = np.argmax(eval_score)
-                    max_eval_score_per_model = eval_score[max_eval_score_index]
-                    if max_eval_score_per_model > max_score:
-                        max_score = max_eval_score_per_model
-                        best_candidate = candidates[max_eval_score_index]
-                        best_model = self.models_names[j]
-                else:
-                    logger.warning("No candidates generated for query = {}".format(q))
-            best_results.append({"response": best_candidate, "score": float(max_score), "model": best_model})
-        return best_results
+                    result = self.tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+                    candidates.append(result)
+                except Exception as e:
+                    logger.error(f"Exception : For query = {q}, model = {self.model_name},"
+                                 f"sample # {i},exception = {e}")
+            max_score = -np.inf
+            sorted_candidates = []
+            if len(candidates) > 0:
+                doc_emb = self.eval_embedding_model.encode([q] * n)
+                sum_emb = self.eval_embedding_model.encode(candidates)
+                eval_scores = cosine_similarity(sum_emb, doc_emb)[:, 0]
+                sorted_candidates = [c for _, c in sorted(zip(eval_scores, candidates), reverse=True)]
+            else:
+                logger.warning(f"No candidate for query {q}")
+            return sorted_candidates
+        #
+        #         #
+        #         # max_eval_score_index = np.argmax(eval_score)
+        #         # max_eval_score_per_model = eval_score[max_eval_score_index]
+        #         # if max_eval_score_per_model > max_score:
+        #         #     max_score = max_eval_score_per_model
+        #         #     best_candidate = candidates[max_eval_score_index]
+        #         #     best_model = self.models_names[j]
+        #     else:
+        #         logger.warning("No candidates generated for query = {}".format(q))
+        # best_results.append({"response": best_candidate, "score": float(max_score), "model": best_model})
+        # return best_results
